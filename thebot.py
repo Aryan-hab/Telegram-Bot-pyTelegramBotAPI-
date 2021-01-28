b2f602bae104d425f1fd5bdb25f20e1ff4b92008
Aryan-hab <aryanabdollahi.aa@gmail.com>
2021-01-28 12:24:59 +0330

    Update thebot.py

--- thebot.py
+++ thebot.py
@@ -9,7 +9,7 @@ chars = "abcdefghijklmnopqrstuvwxyz?!@#$%^&*()_+|?\/ "
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Send me your iranian grade and I will convert it to german grade")
+    bot.reply_to(message, "Send me your grade and I will convert it to german grade")
 
 
 @bot.message_handler(content_types=['text'])
@@ -19,7 +19,7 @@ def cal_grade(message):
     chat_id = message.chat.id
     try:
         inp = int(text)
-        bot.send_message(chat_id, "Your Iranian grade is equivalent to : \n\n" + str("{:.2f}".format(pm)) + "\n\nin German grade."
+        bot.send_message(chat_id, "Your grade is equivalent to : \n\n" + str("{:.2f}".format(pm)) + "\n\nin German grade."
         "\n\n\nyou can also check it for yourself using the equation below:\n\n"
         "     7 - (0.3 * your grade)  \n\n\n\n\n"
         "     1 - 1.5 ➡️ 90 - 100\n\n\n     1.51 - 2.5 ➡️ 80 - 90\n\n\n     2.51 - 3.5 ➡️ 70 - 80\n\n\n     3.51 - 4 ➡️ 60 - 70\n\n\n     4.01 - 5➡️ 50-60")
